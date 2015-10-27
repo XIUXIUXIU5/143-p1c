45036a9a7780a0bc237299cbc2f065eda1fd912d
XIUXIUXIU5 <shaolei@ucla.edu>
2015-10-26 23:37:52 -0700

    reindent

--- addActorDirector.php
+++ addActorDirector.php
@@ -1,71 +1,71 @@
 <!DOCTYPE html>
-	<html>
-	<head>
-		<title>add actor / director</title>
-		<style type="text/css">
-		</style>
-	</head>	
-	<body bgcolor="#FFFFAA">
-				Add new actor/director: <br/>
-		<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
-			Identity:	<input type="radio" name="identity" value="Actor" checked="true">Actor
-						<input type="radio" name="identity" value="Director">Director<br/>
-			<hr/>
-			First Name:	<input type="text" name="first" maxlength="20"><br/>
-			Last Name:	<input type="text" name="last" maxlength="20"><br/>
-			Sex:		<input type="radio" name="sex" value="Male" checked="true">Male
-						<input type="radio" name="sex" value="Female">Female<br/>
-						
-			Date of Birth:	<input type="text" name="dob"><br/>
-			Date of Die:	<input type="text" name="dod"> (leave blank if alive now)<br/>
-			<input type="submit" value="add it!!"/>
-		</form>
+<html>
+<head>
+	<title>add actor / director</title>
+	<style type="text/css">
+	</style>
+</head>	
+<body bgcolor="#FFFFAA">
+	Add new actor/director: <br/>
+	<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
+		Identity:	<input type="radio" name="identity" value="Actor" checked="true">Actor
+		<input type="radio" name="identity" value="Director">Director<br/>
 		<hr/>
+		First Name:	<input type="text" name="first" maxlength="20"><br/>
+		Last Name:	<input type="text" name="last" maxlength="20"><br/>
+		Sex:		<input type="radio" name="sex" value="Male" checked="true">Male
+		<input type="radio" name="sex" value="Female">Female<br/>
+		
+		Date of Birth:	<input type="text" name="dob"><br/>
+		Date of Die:	<input type="text" name="dod"> (leave blank if alive now)<br/>
+		<input type="submit" value="add it!!"/>
+	</form>
+	<hr/>
 
 	<?php
-   	if ($_SERVER["REQUEST_METHOD"] == "GET") {
+	if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
-	$db_connection = mysql_connect("localhost:1432", "cs143", ""); 
-	mysql_select_db("CS143",$db_connection);
+		$db_connection = mysql_connect("localhost:1432", "cs143", ""); 
+		mysql_select_db("CS143",$db_connection);
 
 	#Get MaxID First
-    $rs = mysql_query("SELECT id FROM MaxPersonID;",$db_connection);
-	$row = mysql_fetch_row($rs);
-	$newID = intval($row[0])+1;
-	$updateIDQuery = "UPDATE MaxPersonID SET id = " . strval($newID) . " WHERE id = " . strval($newID-1) . ";";
-	$rs = mysql_query($updateIDQuery, $db_connection);
-	if (!$rs) {
-		echo "fail to create id";
-	}
+		$rs = mysql_query("SELECT id FROM MaxPersonID;",$db_connection);
+		$row = mysql_fetch_row($rs);
+		$newID = intval($row[0])+1;
+		$updateIDQuery = "UPDATE MaxPersonID SET id = " . strval($newID) . " WHERE id = " . strval($newID-1) . ";";
+		$rs = mysql_query($updateIDQuery, $db_connection);
+		if (!$rs) {
+			echo "fail to create id";
+		}
 
 	#Get All the attributes
-	$identity = $_GET['identity'];
-	$attributes = array(strval($newID),$_GET['first'],$_GET['last'],$_GET['sex'],$_GET['dob'],$_GET['dod']);
+		$identity = $_GET['identity'];
+		$attributes = array(strval($newID),$_GET['first'],$_GET['last'],$_GET['sex'],$_GET['dob'],$_GET['dod']);
 
     #Set Empty String to NULL & add ' to each attribute
-    for ($i=0; $i < count($attributes); $i++) { 
-    	if(($attributes[$i]) == "") {
-    		$attributes[$i] = "NULL";
-    	}
-    	else 
-    		$attributes[$i] = "'" . $attributes[$i] . "'";
-    }
+		for ($i=0; $i < count($attributes); $i++) { 
+			if(($attributes[$i]) == "") {
+				$attributes[$i] = "NULL";
+			}
+			else 
+				$attributes[$i] = "'" . $attributes[$i] . "'";
+		}
 
     #Create Query
-    $addQuery = "";
-	if($identity == "Actor") {
-		$addQuery = "INSERT INTO Actor (id, last, first, sex, dob, dod) VALUES (";
-		for ($i=0; $i < count($attributes); $i++) { 
-			$addQuery = $addQuery . $attributes[$i];
-			if ($i < count($attributes) - 1) {
-				$addQuery = $addQuery . ",";
-			}
-		}
-		$addQuery = $addQuery . ");";
-	}
+		$addQuery = "";
+		if($identity == "Actor") {
+			$addQuery = "INSERT INTO Actor (id, last, first, sex, dob, dod) VALUES (";
+				for ($i=0; $i < count($attributes); $i++) { 
+					$addQuery = $addQuery . $attributes[$i];
+					if ($i < count($attributes) - 1) {
+						$addQuery = $addQuery . ",";
+					}
+				}
+				$addQuery = $addQuery . ");";
+}
 
-	else if($identity == "Director") {
-		$addQuery = "INSERT INTO Director (id, last, first, dob, dod) VALUES (";
+else if($identity == "Director") {
+	$addQuery = "INSERT INTO Director (id, last, first, dob, dod) VALUES (";
 		for ($i=0; $i < count($attributes); $i++) { 
 			if ($i != 3) #get rid of sex attribute
 			{
@@ -76,21 +76,21 @@
 			}
 		}
 		$addQuery = $addQuery . ");";
-	}
+}
 
-	$rs = mysql_query($addQuery, $db_connection);
-	if ($rs) 
-		echo "successfully added";
-	else {
-		echo "failed to add";
+$rs = mysql_query($addQuery, $db_connection);
+if ($rs) 
+	echo "successfully added";
+else {
+	echo "failed to add";
 		#revert max id if failed
-		$updateIDQuery = "UPDATE MaxPersonID SET id = " . strval($newID-1) . " WHERE id = " . strval($newID) . ";";
-		$rs = mysql_query($updateIDQuery, $db_connection);
-	}
-    mysql_free_result($rs);
-    mysql_close($db_connection);
-	}
-	?>
-				
-	</body>
+	$updateIDQuery = "UPDATE MaxPersonID SET id = " . strval($newID-1) . " WHERE id = " . strval($newID) . ";";
+	$rs = mysql_query($updateIDQuery, $db_connection);
+}
+mysql_free_result($rs);
+mysql_close($db_connection);
+}
+?>
+
+</body>
 </html>
--- addMovieInfo.php
+++ addMovieInfo.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html>
-	<html>
-	<head>
-		<title>add new movie</title>
-		<style type="text/css">
-		</style>
-	</head>	
-	<body bgcolor="#FFFFAA">
-				Add new movie: <br/>
-		<form action="./addMovieInfo.php" method="GET">			
-			Title : <input type="text" name="title" maxlength="20"><br/>
-			Compnay: <input type="text" name="company" maxlength="50"><br/>
-			Year : <input type="text" name="year" maxlength="4"><br/>	<!-- Todo: validation-->	
-			MPAA Rating : <select name="mpaarating">
-					<option value="G">G</option>
-					<option value="NC-17">NC-17</option>
-					<option value="PG">PG</option>
-					<option value="PG-13">PG-13</option>
-					<option value="R">R</option>
-					<option value="surrendere">surrendere</option>
-			</select>		
-			<br/>
-			<input type="submit" value="Add it!!"/>
-		</form>
-	<hr/>
+<html>
+<head>
+	<title>add new movie</title>
+	<style type="text/css">
+	</style>
+</head>	
+<body bgcolor="#FFFFAA">
+	Add new movie: <br/>
+	<form action="./addMovieInfo.php" method="GET">			
+		Title : <input type="text" name="title" maxlength="20"><br/>
+		Compnay: <input type="text" name="company" maxlength="50"><br/>
+		Year : <input type="text" name="year" maxlength="4"><br/>	<!-- Todo: validation-->	
+		MPAA Rating : <select name="mpaarating">
+		<option value="G">G</option>
+		<option value="NC-17">NC-17</option>
+		<option value="PG">PG</option>
+		<option value="PG-13">PG-13</option>
+		<option value="R">R</option>
+		<option value="surrendere">surrendere</option>
+	</select>		
+	<br/>
+	<input type="submit" value="Add it!!"/>
+</form>
+<hr/>
 
-	<?php
-   	if ($_SERVER["REQUEST_METHOD"] == "GET") {
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
 	$db_connection = mysql_connect("localhost:1432", "cs143", ""); 
 	mysql_select_db("CS143",$db_connection);
 
 	#Get MaxID First
-    $rs = mysql_query("SELECT id FROM MaxMovieID;",$db_connection);
+	$rs = mysql_query("SELECT id FROM MaxMovieID;",$db_connection);
 	$row = mysql_fetch_row($rs);
 	$newID = intval($row[0])+1;
 	$updateIDQuery = "UPDATE MaxMovieID SET id = " . strval($newID) . " WHERE id = " . strval($newID-1) . ";";
@@ -44,37 +44,37 @@
 	$attributes = array(strval($newID),$_GET['title'],$_GET['year'],$_GET['mpaarating'],$_GET['company']);
 
     #Set Empty String to NULL & add ' to each attribute
-    for ($i=0; $i < count($attributes); $i++) { 
-    	if(($attributes[$i]) == "") {
-    		$attributes[$i] = "NULL";
-    	}
-    	else 
-    		$attributes[$i] = "'" . $attributes[$i] . "'";
-    }
+	for ($i=0; $i < count($attributes); $i++) { 
+		if(($attributes[$i]) == "") {
+			$attributes[$i] = "NULL";
+		}
+		else 
+			$attributes[$i] = "'" . $attributes[$i] . "'";
+	}
 
     #Create Query
 	$addQuery = "INSERT INTO Movie (id, title, year, rating, company) VALUES (";
-	for ($i=0; $i < count($attributes); $i++) { 
-		$addQuery = $addQuery . $attributes[$i];
-		if ($i < count($attributes) - 1) {
-			$addQuery = $addQuery . ",";
+		for ($i=0; $i < count($attributes); $i++) { 
+			$addQuery = $addQuery . $attributes[$i];
+			if ($i < count($attributes) - 1) {
+				$addQuery = $addQuery . ",";
+			}
 		}
-	}
-	$addQuery = $addQuery . ");";
-	
-	$rs = mysql_query($addQuery, $db_connection);
-	if ($rs) 
-		echo "successfully added";
-	else {
-		echo "failed to add";
+		$addQuery = $addQuery . ");";
+
+$rs = mysql_query($addQuery, $db_connection);
+if ($rs) 
+	echo "successfully added";
+else {
+	echo "failed to add";
 		#revert max id if failed
-		$updateIDQuery = "UPDATE MaxMovieID SET id = " . strval($newID-1) . " WHERE id = " . strval($newID) . ";";
-		$rs = mysql_query($updateIDQuery, $db_connection);
-	}
-    mysql_free_result($rs);
-    mysql_close($db_connection);
-	}
-	?>
-				
-	</body>
+	$updateIDQuery = "UPDATE MaxMovieID SET id = " . strval($newID-1) . " WHERE id = " . strval($newID) . ";";
+	$rs = mysql_query($updateIDQuery, $db_connection);
+}
+mysql_free_result($rs);
+mysql_close($db_connection);
+}
+?>
+
+</body>
 </html>
