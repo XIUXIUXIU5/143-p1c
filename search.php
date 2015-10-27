ea7f82957240e217e4c665286dfb648ff655aefd
XIUXIUXIU5 <shaolei@ucla.edu>
2015-10-27 11:14:34 -0700

    add search

--- search.php
+++ search.php
@@ -1,23 +1,62 @@
 <!DOCTYPE html>
-	<html>
-	<head>
-		<title>Search Actor / Movie</title>
-		<style type="text/css">
-		</style>
-	</head>	
-	<body bgcolor="#FFFFAA">
-			
-		Search for actors/movies
-	  	<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
-			Search: <input type="text" name="keyword"></input>
-			<input type="submit" value="Search"/>
-		</form>
-		<hr/>
+<html>
+<head>
+	<title>Search Actor / Movie</title>
+	<style type="text/css">
+	</style>
+</head>	
+<body bgcolor="#FFFFAA">
+
+	Search for actors/movies
+	<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
+		Search: <input type="text" name="keyword"></input>
+		<input type="submit" value="Search"/>
+	</form>
+	<hr/>
 	<?php
-   	if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $keyword = $_GET['keyword'];
-    echo "<br>";
+	if ($_SERVER["REQUEST_METHOD"] == "GET") {
+		$keyword = $_GET['keyword'];
+		$db_connection = mysql_connect("localhost:1432", "cs143", ""); 
+		mysql_select_db("CS143",$db_connection);
+
+		echo "You are searching [".$keyword."] results...";
+		echo "<br>";
+		echo "<br>";
+
+		echo "Searching match records in Actor database ... ";
+		echo "<br>";
+
+		$actorQuery = "SELECT * FROM Actor WHERE first LIKE '%". $keyword."%' OR last LIKE '%".$keyword."%';";
+
+		$rs = mysql_query($actorQuery,$db_connection);
+
+		while ($row = mysql_fetch_array($rs)) {
+			echo "Actor: ";
+			$text = $row[2]." ".$row[1] . "(".$row[4] . ")";
+			echo "<a href='showActorInfo.php?aid=".$row[0]."'>".$text."</a>";
+			echo "<br/>";
+		}
+
+		echo "<br/>";
+		echo "Searching match records in Movie database ... ";
+
+		$movieQuery = "SELECT * FROM Movie WHERE title LIKE '%". $keyword."%';";
+		echo "<br>";
+
+		$rs = mysql_query($movieQuery,$db_connection);
+
+		while ($row = mysql_fetch_array($rs)) {
+			echo "Movie: ";
+			$text = $row[1]."(".$row[2] . ")";
+			echo "<a href='showMovieInfo.php?aid=".$row[0]."'>".$text."</a>";
+			echo "<br/>";
+		}
+
+
+		mysql_free_result($rs);
+		mysql_close($db_connection);
+
 	}
 	?>
-	</body>
+</body>
 </html>
