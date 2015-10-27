d94c561b09a88d51321e883272dbd3038be6ec93
XIUXIUXIU5 <shaolei@ucla.edu>
2015-10-27 00:36:44 -0700

    addMovieDirector & add todo to addMovieInfo

--- addMovieDirector.php
+++ addMovieDirector.php
@@ -1,25 +1,80 @@
 <!DOCTYPE html>
-	<html>
-	<head>
-		<title>Add a director to a movie</title>
-		<style type="text/css">
-		</style>
-	</head>	
-	<body bgcolor="#FFFFAA">
-				Add new director to a movie: <br/>
-		<form action="./addMovieDirector.php" method="GET">
-					
-			
-			Movie : <select name="mid">
-								</select>
-			<br/>		
-			Director : <select name="aid">
-					</select>
-			<br/>	
-			<input type="submit" value="Add it!!"/>
-					</form>
-		<hr/>
-				
-	</body>
+<html>
+<head>
+	<title>Add a director to a movie</title>
+	<style type="text/css">
+	</style>
+</head>	
+<body bgcolor="#FFFFAA">
+	Add new director to a movie: <br/>
+	<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
+		<?php
+		$db_connection = mysql_connect("localhost:1432", "cs143", ""); 
+		mysql_select_db("CS143",$db_connection);
+
+		$rs = mysql_query("SELECT * FROM Movie;",$db_connection);
+
+		echo "Movie : <select name=mid>";
+		while ($row = mysql_fetch_array($rs)) {
+			echo "<option value='" . $row['0'] . "'>" . $row[1] . "(" . $row[2] . ")" . "</option>";
+		}
+		echo "</select>";
+
+		mysql_free_result($rs);
+		mysql_close($db_connection);
+
+		?>		
+
+		<br/>		
+		
+		<?php
+		$db_connection = mysql_connect("localhost:1432", "cs143", ""); 
+		mysql_select_db("CS143",$db_connection);
+
+		$rs = mysql_query("SELECT * FROM Director;",$db_connection);
+
+		echo "Director : <select name=did>";
+		while ($row = mysql_fetch_array($rs)) {
+			echo "<option value='" . $row['0'] . "'>" . $row[1] . $row[2] . "(" . $row[3] . ")" . "</option>";
+		}
+		echo "</select>";
+
+		mysql_free_result($rs);
+		mysql_close($db_connection);
+
+		?>		
+	</select>
+	<br/>	
+	<input type="submit" value="Add it!!"/>
+</form>
+<hr/>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+	$db_connection = mysql_connect("localhost:1432", "cs143", ""); 
+	mysql_select_db("CS143",$db_connection);
+
+	$checkQuery = "SELECT * FROM MovieDirector WHERE mid = '" . $_GET['mid'] . "' AND did = '" . $_GET['did']."';";
+	$rs = mysql_query($checkQuery, $db_connection);
+	$row = mysql_fetch_array($rs);
+	if($row)
+		echo "This relationship has been added already";
+	else {
+		$addQuery = "INSERT INTO MovieDirector(mid, did) VALUES (" . "'" . $_GET['mid'] ."'".","."'".$_GET['did']."');";
+		$rs = mysql_query($addQuery, $db_connection);
+		if ($rs) 
+			echo "successfully added";
+		else {
+			echo "failed to add";
+		}
+	}
+	mysql_free_result($rs);
+	mysql_close($db_connection);
+}
+?>
+
+
+</body>
 </html>
 
--- addMovieInfo.php
+++ addMovieInfo.php
@@ -7,7 +7,7 @@
 </head>	
 <body bgcolor="#FFFFAA">
 	Add new movie: <br/>
-	<form action="./addMovieInfo.php" method="GET">			
+	<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
 		Title : <input type="text" name="title" maxlength="20"><br/>
 		Compnay: <input type="text" name="company" maxlength="50"><br/>
 		Year : <input type="text" name="year" maxlength="4"><br/>	<!-- Todo: validation-->	
@@ -20,12 +20,37 @@
 		<option value="surrendere">surrendere</option>
 	</select>		
 	<br/>
+	Genre : 
+	<input type="checkbox" name="genre_Action" value="Action">Action</input>
+	<input type="checkbox" name="genre_Adult" value="Adult">Adult</input>
+	<input type="checkbox" name="genre_Adventure" value="Adventure">Adventure</input>
+	<input type="checkbox" name="genre_Animation" value="Animation">Animation</input>
+	<input type="checkbox" name="genre_Comedy" value="Comedy">Comedy</input>
+	<input type="checkbox" name="genre_Crime" value="Crime">Crime</input>
+	<input type="checkbox" name="genre_Documentary" value="Documentary">Documentary</input>
+	<input type="checkbox" name="genre_Drama" value="Drama">Drama</input>
+	<input type="checkbox" name="genre_Family" value="Family">Family</input>
+	<input type="checkbox" name="genre_Fantasy" value="Fantasy">Fantasy</input>
+	<input type="checkbox" name="genre_Horror" value="Horror">Horror</input>
+	<input type="checkbox" name="genre_Musical" value="Musical">Musical</input>
+	<input type="checkbox" name="genre_Mystery" value="Mystery">Mystery</input>
+	<input type="checkbox" name="genre_Romance" value="Romance">Romance</input>
+	<input type="checkbox" name="genre_Sci-Fi" value="Sci-Fi">Sci-Fi</input>
+	<input type="checkbox" name="genre_Short" value="Short">Short</input>
+	<input type="checkbox" name="genre_Thriller" value="Thriller">Thriller</input>
+	<input type="checkbox" name="genre_War" value="War">War</input>
+	<input type="checkbox" name="genre_Western" value="Western">Western</input>
+	<br/>
 	<input type="submit" value="Add it!!"/>
 </form>
 <hr/>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+
+	#TODO:
+	#Add genre
 
 	$db_connection = mysql_connect("localhost:1432", "cs143", ""); 
 	mysql_select_db("CS143",$db_connection);
